27bb98beef1074f1289e302cc865f827ec6c01c1
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2022-09-07 14:08:31 +0200

    + fix ebale

--- install.sh
+++ install.sh
@@ -9,11 +9,12 @@ SCRIPT_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
 cd $SCRIPT_PATH
 
 _DOWNLOADS=downloads
-_TAR_TARGET=alermanager
+_TAR_TARGET=alertmanager
 _LATEST_RELEASE=`curl -s https://api.github.com/repos/prometheus/alertmanager/releases/latest | grep browser_download_url | grep "linux-amd64" | awk '{print $2}' | tr -d '\"'`
 # http://127.0.0.1:9087/alert/<chat_ID> or use another URL like a Slack hook
 _ALERT_URL=http://127.0.0.1:9087/alert/
-_SERVER_IP=`hostname -I`
+_SERVER_IP=`/sbin/ifconfig eth0 | grep 'inet ' | awk '{ print $2}'`
+_USER=alertmanager
 # Fncs
 # ---------------------------------------------------\
 
@@ -46,28 +47,52 @@ final_steps() {
     echo -e "Please update Alertmanager URL in /etc/prometheus/prometheus.yml"
 }
 
+check_dir() {
+
+  if [[ ! -d "$1" ]]; then
+    mkdir -p $1
+  fi
+
+}
+
 install_alertmanager() {
 
     cp  pkg/bin/* /usr/local/bin
-    mkdir -p /etc/alertmanager
-    mkdir -p /data/alertmanager
-    useradd -rs /bin/false alertmanager
+    check_dir /etc/alertmanager
+    check_dir /data/alertmanager
 
-    mv alertmanager.yml /etc/alertmanager/
+    if id -u "$_USER" >/dev/null 2>&1; then
+        echo "User: $_USER already exists..."
+    else
+        echo "Creating user: $_USER .."
+        useradd -rs /bin/false alertmanager
+    fi
+    
+    push_template pkg/alertmanager_conf.tmpl /etc/alertmanager/alertmanager.yml
+    push_template pkg/alertmanager_unit.tmpl /lib/systemd/system/alertmanager.service
 
-    chown -R alertmanager:alertmanager /data/alertmanager /etc/alertmanager/*
+    chown -R alertmanager:alertmanager /data/alertmanager /etc/alertmanager
     chown alertmanager:alertmanager /usr/local/bin/amtool /usr/local/bin/alertmanager
+
+    systemctl daemon-reload
+    systemctl enable --now alertmanager
 
 }
 
 download_release() {
 
+    check_dir $SCRIPT_PATH/tmp
+    check_dir $SCRIPT_PATH/pkg/bin/
+
     if [[ ! -d "$_DOWNLOADS" ]]; then
+        mkdir -p "$_DOWNLOADS/$_TAR_TARGET"
+      else
         rm -rf "$_DOWNLOADS"
         mkdir -p "$_DOWNLOADS/$_TAR_TARGET"
     fi
 
     cd "$_DOWNLOADS"
+    echo "Download latest alertmanager release from GitHub..."
     wget "$_LATEST_RELEASE"
     tar xvf alertmanager*amd64.tar.gz --directory "$_TAR_TARGET"/
 
@@ -76,6 +101,7 @@ download_release() {
     local _BINARY_TOOL="$_DOWNLOADS"/"$_TAR_TARGET"/"$_BINARY_CATALOG"/amtool
     
     cd $SCRIPT_PATH
+
     cp "$_BINARY" pkg/bin/
     cp "$_BINARY_TOOL" pkg/bin/
 
